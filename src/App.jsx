88e29528a0b509f7b80a2853ecbbef4d688bea8e
Armando Ruiz <armando.ruiz@industrie.co>
2022-05-31 15:22:25 +1000

    95. Creando el formulario en Nuevo Presupuesto y creando el state de presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import Header from './components/Header'
 
 function App() {
 
+  // como el presupuesto aunque se defina en el componente nuevoPresupuesto, se va a usar en otros componentes,
+  // se declara en el componente principal para q todos lo puedan usar.
+  const [presupuesto, setPresupuesto] = useState(0);
+
   return (
     <div>
-      <Header />
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </div>
   )
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
       <form className="formulario">
@@ -10,6 +10,8 @@ const NuevoPresupuesto = () => {
             className='nuevo-presupuesto'
             type="text"
             placeholder='Agrega tu Presupuesto'
+            value={presupuesto}
+            onChange={ (e) => setPresupuesto(e.target.value) }
           />
         </div>
 
